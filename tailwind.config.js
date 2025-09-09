ef09900f9377625b011c813b0a48098c92638c54
etgbajy-spec <etgbajy@gmail.com>
2025-09-09 22:19:04 +0900

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./*.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./dist/**/*.{js,css}"
+    "./**/*.html",
+    "./**/*.js"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#fdf2f8',
-          100: '#fce7f3',
-          200: '#fbcfe8',
-          300: '#f9a8d4',
-          400: '#f472b6',
-          500: '#ec4899',
-          600: '#db2777',
-          700: '#be185d',
-          800: '#9d174d',
-          900: '#831843',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
